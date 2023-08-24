dec75f125d79e7497f4130cbe1b1d2a9e95e4e8a
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-08-24 13:00:22 +0300

    Fix rubocop errors.

--- my_list.rb
+++ my_list.rb
@@ -2,15 +2,15 @@ require_relative 'enumerable'
 
 
 class MyList
-    include MyEnumerable
-    def initialize(*list)
-      @list = list
-    end
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
 
-    def each
-        @list.each {|i| yield(i)}
-      end
+  def each
+    @list.each { |i| yield(i) }
+  end
 end
 
-list = MyList.new(1,2,3,4)
+list = MyList.new(1, 2, 3, 4)
 puts list.all?
