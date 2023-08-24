ddaf49fe9a87f4576246f752fc736a85cf02ab28
Batool Fatima <batoolfatima2135@gmail.com>
2023-08-24 15:02:17 +0500

    Check code

--- my_list.rb
+++ my_list.rb
@@ -13,4 +13,7 @@ class MyList
 end
 
 list = MyList.new(1, 2, 3, 4)
-puts list.all?
+puts list.all? {|e| e < 5}
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
+puts list.filter {|e| e.even?}
